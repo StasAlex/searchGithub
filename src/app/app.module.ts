c19872f6344c5d4b4cfb5c875ba5bd6fe6aacf60
StasAlex <stas.alex28@gmail.com>
2020-01-16 20:25:30 +0200

    add template for github users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { SearchComponent } from './components/search/search.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SearchComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    HttpClientModule,
+    NgbModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap, pluck } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  formValue = '';
+  url = 'https://api.github.com/search/users?q=';
+  userForm = new FormGroup({
+    userName: new FormControl('')
+  });
 
-  constructor() { }
+  userStream$: any = this.userForm.valueChanges.pipe(
+    debounceTime(1000), // задерживает поток на 1000мс
+    pluck('userName'), // берет из потока ключи полей userName
+    distinctUntilChanged(), // сравнивает элемент потока с предыдущим
+    // и испускает только значения, отличающиеся от предыдущего
+    switchMap(v => this.http.get(this.url + v)), // формирует новый поток  - берет значение
+    // из предыдущего и вставляет в новый поток
+    pluck('items') // берет из потока ключи полей items
+  );
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
